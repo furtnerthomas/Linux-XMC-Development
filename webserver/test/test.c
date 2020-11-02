bd614100d8b972a961d9ae1a5f3373073f90cd6b
Thomas Furtner <thomas.furtner@infineon.com>
2020-11-02 20:30:14 +0100

    add test

---
+++ webserver/test/test.c
@@ -0,0 +1,13 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+  printf("1..1\n");
+  printf("ok 1 - Swallows fly\n");
+  /* printf("'not ok 2 - Bummer, this test has failed.'\n"); */
+  /* printf("'ok 3 - Swallows fly'\n"); */
+  return 0;
+}
+
